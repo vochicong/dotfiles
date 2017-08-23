bfd76c2253e582e87687ee5746b60fe6cd84e18d
Cong <kongvc@gmail.com>
2017-08-23 17:31:44 +0900

    Add vim settings

---
+++ .vimrc
@@ -0,0 +1,8 @@
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+
+"autocmd vimenter * NERDTree
+map <C-n> :NERDTreeToggle<CR>
+let g:NERDTreeGlyphReadOnly = "RO"
+
---
+++ install-vim-bundles.sh
@@ -0,0 +1,6 @@
+mkdir -p  ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git
+
